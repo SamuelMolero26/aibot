1cd27e11dabd903b48ddba844c7dea67abbba10f
SamuelMolero26 <samueljosemolero63@gmail.com>
2024-01-28 00:47:57 -0600

    new commit

--- app/server.py
+++ app/server.py
@@ -6,9 +6,8 @@ app = Flask(__name__)
 def home():
     return render_template('login_register.html')
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    return render_template('login.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
